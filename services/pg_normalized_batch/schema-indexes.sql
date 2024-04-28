41add4176112f9cc4a41ae96279d5a034a003f37
ciherrera20 <ciherrera20@icloud.com>
2024-04-27 17:44:01 -0700

    Commented normalized_batch's schema-indexes.sql

--- services/pg_normalized_batch/schema-indexes.sql
+++ services/pg_normalized_batch/schema-indexes.sql
@@ -1,2 +1,5 @@
+/*
+ * I have less indices because I added back the UNIQUE constraints to schema.sql 
+ */
 CREATE INDEX ON tweet_tags(tag text_pattern_ops, id_tweets);
 CREATE INDEX ON tweets USING GIN(to_tsvector('english', text));
