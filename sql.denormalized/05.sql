a3b20d1565b30267539dda62b689c18c7af99323
ciherrera20 <ciherrera20@icloud.com>
2024-04-27 17:38:14 -0700

    Simplified some of the denormalized queries and created better indices

--- sql.denormalized/05.sql
+++ sql.denormalized/05.sql
@@ -8,25 +8,19 @@ FROM (
     SELECT DISTINCT
         data->>'id' AS id_tweets,
         '#'::text || (jsonb_array_elements(
-             COALESCE(data->'entities'->'hashtags', '[]') ||
-             COALESCE(data->'extended_tweet'->'entities'->'hashtags', '[]')
-         )->>'text') AS tag
-    FROM tweets_jsonb
-    WHERE 
-        to_tsvector('english',data->'extended_tweet'->>'full_text')@@to_tsquery('english','coronavirus') AND
-        data->>'lang'='en'
-    UNION ALL
-    SELECT DISTINCT
-        data->>'id' AS id_tweets,
-        '#'::text || (jsonb_array_elements(
             COALESCE(data->'entities'->'hashtags', '[]') ||
             COALESCE(data->'extended_tweet'->'entities'->'hashtags', '[]')
         )->>'text') AS tag
     FROM tweets_jsonb
-    WHERE 
-        data->'extended_tweet'->>'full_text' IS NULL AND
-        to_tsvector('english',data->>'text')@@to_tsquery('english','coronavirus') AND
-        data->>'lang'='en'
+    WHERE
+        (
+            to_tsvector('english',data->'extended_tweet'->>'full_text')@@to_tsquery('english','coronavirus') OR
+            (
+                data->'extended_tweet'->>'full_text' IS NULL AND
+                to_tsvector('english',data->>'text')@@to_tsquery('english','coronavirus')
+            )
+        ) AND
+        data->'lang'?'en'
 ) t
 GROUP BY tag
 ORDER BY count DESC,tag
